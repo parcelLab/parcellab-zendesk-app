1667bddc1cb3a0f1a5c9266d808426af0c9ef106
Andre Mueller <andre@parcellab.com>
2019-11-08 16:45:14 +0000

    Add ExceptionNotification unit tests

---
+++ spec/components/exceptionnotification.spec.js
@@ -0,0 +1,28 @@
+/* eslint-env jest, browser */
+import React from 'react'
+import { render, fireEvent, wait } from '@testing-library/react'
+
+import ExceptionNotification from '../../src/javascripts/modules/components/exceptionnotification'
+
+describe('ExceptionNotification Component', () => {
+  const exception = {
+    type: 'error',
+    message: 'erorr message'
+  }
+  const onClose = jest.fn()
+
+  it('should display exception message', async () => {
+    const { queryByText } = render(<ExceptionNotification exception={exception} onClose={onClose} />)
+
+    await wait(() => {
+      expect(queryByText(exception.message)).toBeInTheDocument()
+    })
+  })
+
+  it('should call onClose if exception notification close button is triggered ', async () => {
+    const { container } = render(<ExceptionNotification exception={exception} onClose={onClose} />)
+
+    fireEvent.click(container.querySelector('button'))
+    expect(onClose).toHaveBeenCalled()
+  })
+})
